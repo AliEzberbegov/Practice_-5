bfa932488bbfeffb2ebf9c3f72318c3fe2195732
AliEzberbegov <oneberg.2002@gmail.com>
2024-12-21 16:51:51 +0300

    Практика №8

--- src/test/java/ibs/practice/utils/WebDriverManager.java
+++ src/test/java/ibs/practice/utils/WebDriverManager.java
@@ -17,9 +17,9 @@ import java.util.Properties;
 
 public class WebDriverManager {
 
-    // Храним настройки в статическом блоке, чтобы прочитать их один раз при загрузке класса
     private static final Properties props = new Properties();
 
+    // Статический блок: подгружаем config.properties один раз
     static {
         try (InputStream input = new FileInputStream("src/test/resources/config.properties")) {
             props.load(input);
@@ -28,48 +28,59 @@ public class WebDriverManager {
         }
     }
 
+    /**
+     * Создаёт WebDriver в зависимости от типа запуска:
+     *  - local: ChromeDriver или FirefoxDriver
+     *  - remote: Selenoid (RemoteWebDriver + DesiredCapabilities)
+     */
     public static WebDriver createDriver() {
-
-        // Сначала ищем переменные в System properties (переданные из Jenkins командой -Dtype.run=remote, и т.д.).
-        // Если таких нет — читаем из config.properties (props).
+        // Считываем настройки из System.getProperty(...) или, если нет - берем из props
         String runType = System.getProperty("type.run", props.getProperty("type.run", "local"));
         String browser = System.getProperty("type.browser", props.getProperty("type.browser", "chrome"));
-        String selenoidUrl = System.getProperty("selenoid.url", props.getProperty("selenoid.url", "http://localhost:4444/wd/hub"));
         String browserVersion = System.getProperty("browser.version", props.getProperty("browser.version", "109.0"));
+        String selenoidUrl = System.getProperty("selenoid.url", props.getProperty("selenoid.url", "http://localhost:4444/wd/hub"));
+
+        // enableVNC / enableVideo (true/false) - читаем как boolean
+        boolean enableVNC = Boolean.parseBoolean(
+                System.getProperty("enableVNC", props.getProperty("enableVNC", "true"))
+        );
+        boolean enableVideo = Boolean.parseBoolean(
+                System.getProperty("enableVideo", props.getProperty("enableVideo", "false"))
+        );
+
+        // Пути к локальным драйверам
+        String chromePath = System.getProperty("chrome.driver.path", props.getProperty("chrome.driver.path", "src/test/resources/chromedriver.exe"));
+        String geckoPath = System.getProperty("gecko.driver.path", props.getProperty("gecko.driver.path", "src/test/resources/geckodriver.exe"));
 
         WebDriver driver;
 
-        // Определяем — локальный запуск или через Selenoid
         if ("local".equalsIgnoreCase(runType)) {
-
-            // Локальный запуск
+            // --- Локальный запуск ---
             switch (browser.toLowerCase()) {
                 case "chrome":
-                    System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+                    System.setProperty("webdriver.chrome.driver", chromePath);
                     driver = new ChromeDriver();
                     break;
-
                 case "firefox":
-                    System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
+                    System.setProperty("webdriver.gecko.driver", geckoPath);
                     driver = new FirefoxDriver();
                     break;
-
                 default:
                     throw new IllegalArgumentException("Неизвестный браузер для локального запуска: " + browser);
             }
 
         } else {
-            // Удалённый запуск (Selenoid)
+            // --- Удалённый запуск (Selenoid) ---
             DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("browserName", browser);      // chrome, firefox и т.д.
+            capabilities.setCapability("browserName", browser); // chrome / firefox / etc.
             capabilities.setCapability("browserVersion", browserVersion);
 
-            // Параметры для Selenoid
+            // Параметры Selenoid
             Map<String, Object> selenoidOptions = new HashMap<>();
-            selenoidOptions.put("enableVNC", true);
-            selenoidOptions.put("enableVideo", false);
+            selenoidOptions.put("enableVNC", enableVNC);
+            selenoidOptions.put("enableVideo", enableVideo);
 
-            // Важно: в новых версиях Selenium нужно именно "selenoid:options"
+            // В новых версиях Selenium использовать именно "selenoid:options"
             capabilities.setCapability("selenoid:options", selenoidOptions);
 
             try {
@@ -78,14 +89,12 @@ public class WebDriverManager {
                         capabilities
                 );
             } catch (MalformedURLException e) {
-                throw new RuntimeException("Некорректный адрес Selenoid: " + selenoidUrl, e);
+                throw new RuntimeException("Некорректный URL для Selenoid: " + selenoidUrl, e);
             }
         }
 
-        // Общие настройки веб-драйвера
         driver.manage().window().maximize();
-        // Вы можете дополнительно установить timeouts и т.д. при желании
-
+        // При желании можете здесь выставить timeouts
         return driver;
     }
 }
